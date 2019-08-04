d15968b4cf517fd09ab7ad22b997fc4578602806
Trey <treytonbooger@gmail.com>
2019-08-04 16:57:04 -0600

    Saving process
    Broken saving process

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,7 @@ namespace FPS_Booster // False name, Computer_Crasher would be better. Atleast f
 {
     public partial class Form1 : Form
     {
+        bool killon = false;
         Process[] pArry = Process.GetProcesses(); // creates process array
         public Form1()
         {
@@ -40,8 +41,8 @@ namespace FPS_Booster // False name, Computer_Crasher would be better. Atleast f
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            int indx = listBox1.SelectedIndex; // refreshes list, i'll add a timer later
-            Process proc = pArry[indx]; // creates proc index array
+            int indx = listBox1.SelectedIndex; // gets selected item
+            Process proc = pArry[indx]; // selected index is now equal to a process index
             if (proc.HasExited == false) // checks if process has exited
             {
                 proc.Kill(); // kills process
@@ -59,6 +60,56 @@ namespace FPS_Booster // False name, Computer_Crasher would be better. Atleast f
                 {
                     listBox1.SetSelected(i, true);
                 }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem); // adds item to listbox2, no actual  saving function yet
+        }
+
+        private void Button4_Click(object sender, EventArgs e) // remove function for listbox2
+        {
+            int indx = listBox2.SelectedIndex; 
+            listBox2.Items.RemoveAt(indx);
+        }
+
+        private void Button5_Click(object sender, EventArgs e) // kills all processes
+        {
+            string itemname = "";
+            foreach (var item in listBox2.Items)
+            {
+                itemname = item.ToString();
+                foreach (var process in Process.GetProcessesByName(itemname))
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e) // kills all processes, but its on a timer
+        {
+            string itemname = "";
+            foreach (var item in listBox2.Items)
+            {
+                itemname = item.ToString();
+                foreach (var process in Process.GetProcessesByName(itemname))
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        private void Button6_Click(object sender, EventArgs e) // enables and disables timer1.
+        {
+            killon = !killon;
+
+            if (killon == true)
+            {
+                timer1.Start();
+            } else if (killon == false)
+            {
+                timer1.Stop();
             }
         }
     }
