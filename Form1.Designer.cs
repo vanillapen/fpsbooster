88dcaf34e46df82bbb10cedbe1c9f08486bf5d72
Trey <treytonbooger@gmail.com>
2019-08-03 18:53:30 -0600

    Basic Code
    Added some basic code, read notes.
    
    Pretty much task manager right now,  but this will be changed later.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(122, 407);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Processes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kill Selected";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(249, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "FPS Booster";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
-namespace FPS_Booster
+/* This program is not done, and really just decreases FPS. It's kinda like an ugly barely working needed admin privilages task manager, which will be switched later
+ The basic code is down, and if you spend 10 minutes you could make it an effective fps booster
+ be warned, this could crash your computer as some really shitty coding practices have taken place, out of pure lazyness. you have been warned.
+ */
+
+namespace FPS_Booster // False name, Computer_Crasher would be better. Atleast for now.
 {
     public partial class Form1 : Form
     {
+        Process[] pArry = Process.GetProcesses(); // creates process array
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true; // alphabetically sorts listbox
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            foreach (Process p in pArry) // for each, in array - pretty straight foward.
+            {
+                string s = p.ProcessName; // gets process name, and sets to string
+                s = s.ToLower(); // lowercases processname
+                listBox1.Items.Add(p.ProcessName); // adds process name to list, need to change this to a list checkbox
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            int indx = listBox1.SelectedIndex; // refreshes list, i'll add a timer later
+            Process proc = pArry[indx]; // creates proc index array
+            if (proc.HasExited == false) // checks if process has exited
+            {
+                proc.Kill(); // kills process
+                proc.Dispose(); // disposes process
+                listBox1.Items.RemoveAt(indx); //removes selected process from list, after it has been killed and disposed of
+            }
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e) // search bar, will change from textbox1 later.
+        {
+            listBox1.SelectedItems.Clear();  // clears list, only shows items with searched letter
+            for (int i = listBox1.Items.Count -1; i >= 0;i--) 
+            {
+                if (listBox1.Items[i].ToString().ToLower().Contains(textBox1.Text)) // does so many things, but mainly adds the searched items back to he list
+                {
+                    listBox1.SetSelected(i, true);
+                }
+            }
         }
     }
 }
